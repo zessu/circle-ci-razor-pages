b504ce9bd55a8461397e53b821a39efdc2855a4d
zessu <amackzie@gmail.com>
2019-12-03 21:26:25 +0300

    (feat): finish view all todo items page

--- Pages/ViewToDoItems.cshtml.cs
+++ Pages/ViewToDoItems.cshtml.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using circle_ci_asp_net_razor_pages.Data;
+using circle_ci_asp_net_razor_pages.Models;
+using System.Linq;
 
 namespace circle_ci_asp_net_razor_pages.Pages
 {
   public class ViewToDoItemsModel : PageModel
   {
+
+    public List<Todo> ToDoItems { get; set; }
+    public DatabaseContext Context { get; }
+
+    public ViewToDoItemsModel(DatabaseContext context)
+    {
+      Context = context;
+    }
     public void OnGet()
     {
+      ToDoItems = Context.Todo.ToList();
     }
   }
 }
